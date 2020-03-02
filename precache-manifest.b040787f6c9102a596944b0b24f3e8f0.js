db717ba8637f8eb69d4b33477e6ac26296c85e91
Toth Andras <tot70and@gmail.com>
2020-03-02 19:16:21 +0100

    Updates

---
+++ precache-manifest.b040787f6c9102a596944b0b24f3e8f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b3f9c491a6a85069dbf1bba99f8aa25",
+    "url": "/zany-image/index.html"
+  },
+  {
+    "revision": "0c4ca14f57c822cecef9",
+    "url": "/zany-image/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "4eaaf9efa37e30c646fe",
+    "url": "/zany-image/static/css/main.9f4c013b.chunk.css"
+  },
+  {
+    "revision": "0c4ca14f57c822cecef9",
+    "url": "/zany-image/static/js/2.5f731446.chunk.js"
+  },
+  {
+    "revision": "81c1ab2ba64ac9f85d481dd74a981dc6",
+    "url": "/zany-image/static/js/2.5f731446.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4eaaf9efa37e30c646fe",
+    "url": "/zany-image/static/js/main.d840e37d.chunk.js"
+  },
+  {
+    "revision": "0189025364fd695fa04b",
+    "url": "/zany-image/static/js/runtime-main.c6528e13.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/zany-image/static/media/SEGA.12a3db8b.woff"
+  }
+]);
