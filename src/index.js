6d2585b20c775a45fe651911648c1e339a8c585c
Toth Andras <tot70and@gmail.com>
2020-03-03 18:57:28 +0100

    reudux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 import 'tachyons';
-import App from './containers/App'
+import App from './containers/App';
+import { searchImages } from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(searchImages);
+
+ReactDOM.render(<Provider store={store}>
+	                <App />
+	            </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
